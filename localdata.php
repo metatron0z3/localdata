cd316d980f45c8482b80a6055889a4c48e64069b
Pavel Kukov <pavelkukov@gmail.com>
2012-07-03 12:45:07 +0300

    update

--- localdata.php
+++ localdata.php
@@ -1,4 +1,29 @@
 <?php
+/*
+ * localdata.php
+ *
+ * @author Pavel Kukov
+ * 
+ * date: 2012-07-02 18:41:11
+ *
+ *
+ *	Sample usage for localdata.php 
+ *	Include - include('localdata.php');
+ *	 
+ *	Create Object - $localdata = localdata::get(); 
+ *	
+ *	Store - $localdata("name", "value"); or $localdata->name = "value"; value can be integer, string, array
+ *	$localdata("name", array(1,2,3,4,5));
+ *	$localdata("name", array("name" => "John Dow", "email" => "john@company.com")); 
+ *	
+ *	Read - $localdata("name"); or $localdata->name 
+ *	Delete One Item By Key - $localdata->remove("name"); 
+ *	Delete entire cookie - $localdata->clear(); 
+ *	Count Stored Items - $localdata->count(); 
+ *	Get/Set cookie configuration - $localdata->config() and $localdata->config(Array('expires' => 'in days', 'path' => 'your new path', 'domain' => 'domain', 'secure' => true or false}) defaults are 'expires' => 365, 'path' => '/', 'domain' => NULL, 'secure' => NULL 
+ *	Get/Set cookie prefix - $localdata->prefix() and $localdata->prefix("newprefix") default is "localdatacookie" 
+ *
+ */
 class localdata
 {
 	private $cookie_data;
